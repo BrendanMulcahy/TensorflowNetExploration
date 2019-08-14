a31ca25bebca6017d21d5fb513c16113523d2cd2
Brendan Mulcahy <mulcahy.software@gmail.com>
2019-08-13 21:27:59 -0400

    Simple math program

---
+++ SimpleMath/SimpleMath/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using NumSharp;
+using Tensorflow;
+
+namespace SimpleMath
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            RefVariable x = tf.Variable(3, name: "x");
+            RefVariable y = tf.Variable(4, name: "y");
+            Tensor f = (Tensor) x * x * y + y + 2;
+
+            Operation init = tf.global_variables_initializer();
+
+            using (Session session = tf.Session())
+            {
+                session.run(init);
+                NDArray result = f.eval();
+                Console.WriteLine(result.ToString());
+            }
+        }
+    }
+}
